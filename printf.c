84d9cdb15e9df8f18d94ed373cc0f36944666bac
GreenDerrick <greenderrickngugi@gmail.com>
2023-04-23 04:23:46 -0700

    output according to format

--- printf.c
+++ printf.c
@@ -1,51 +1,53 @@
 #include <stdarg.h>
 #include "main.h"
-#include <stdlib.h>
+#include <stddef.h>
 /**
- * _printf - print output
- * @format: variable to be manipulated
- * Return: count
+ * _printf - printing function
+ * @format: variable
+ * Return:count
  */
 int _printf(const char *format, ...)
 {
 	int i = 0, count = 0;
 	va_list print;
 
+	va_start(print, format);
 	if (format != NULL)
 	{
-		va_start(print, format);
 		while (format[i] != '\0')
 		{
-			if (format[i] != '%')
+			if (format[i] == '%')
+			{
+				format++;
+				switch (format[i])
+				{
+					case 'c':
+						_putchar(va_arg(print, int));
+						count = count + 1;
+						break;
+					case 's':
+						_putchar(str(va_arg(print, char*)));
+						count = count + 1;
+						break;
+					case '%':
+						_putchar('%');
+						count = count + 1;
+						break;
+					default:
+						_putchar(format[i]);
+						count = count + 1;
+						break;
+				}
+			}
+			else
 			{
 				_putchar(format[i]);
 				count = count + 1;
 			}
-			else
-			{
-			while (format[i] == '%')
-			{
-				if (format[i + 1] == 'c')
-				{
-					_putchar(va_arg(print, int));
-					count = count + 1;
-				}
-				else if (format[i + 1] == 's')
-				{
-					_putchar(str(va_arg(print, char *)));
-					count = count + 1;
-				}
-				else
-				{
-					_putchar(format[i + 1]);
-					count = count + 1;
-				}
-				i++;
-			}
-			}
-			i++;
+			format++;
 		}
+		va_end(print);
+		return (count);
 	}
-	va_end(print);
-	return (count);
+	return (-1);
 }
