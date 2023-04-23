ee0fb7659252cca42fa7d6a23ff371e73a63f014
samngoru <samngoru@gmail.com>
2023-04-23 11:56:17 -0700

    output according to format

--- main.h
+++ main.h
@@ -3,18 +3,13 @@
 #include <stdarg.h>
 #include <stddef.h>
 /**
- * struct print - prints structure that holds identidiers
- *
  * @c: character c
  * @s: string s
- *
  * Description: will be used to charck for identifiers
  */
-
 int _putchar(char c);
 int _printf(const char *format, ...);
 int print_char(va_list print);
 int print_string(va_list print);
-int print_percent(va_list print);
 int str(char *s);
 #endif
