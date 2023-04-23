25c1ba3a1efbe2f62dc8290427462798d4b4edf3
GreenDerrick <greenderrickngugi@gmail.com>
2023-04-23 03:54:09 -0700

    output according to foramt

--- printf.c
+++ printf.c
@@ -11,44 +11,39 @@ int _printf(const char *format, ...)
 	int i = 0, count = 0;
 	va_list print;
 
-	va_start(print, format);
 	if (format == NULL)
-	{
 		return (-1);
-	}
-	else
+	va_start(print, format);
+	while (format[i] != '\0')
 	{
-		while (format[i] != '\0')
+		if (format[i] != '%')
 		{
-			if (format[i] != '%')
+			_putchar(format[i]);
+			count = count + 1;
+		}
+		else
+		{
+		while (format[i] == '%')
+		{
+			if (format[i + 1] == 'c')
 			{
-				_putchar(format[i]);
+				_putchar(va_arg(print, int));
+				count = count + 1;
+			}
+			else if (format[i + 1] == 's')
+			{
+				_putchar(str(va_arg(print, char *)));
 				count = count + 1;
 			}
 			else
 			{
-				while (format[i] == '%')
-				{
-					if (format[i + 1] == 'c')
-					{
-						_putchar(va_arg(print, int));
-						count = count + 1;
-					}
-					else if (format[i + 1] == 's')
-					{
-						_putchar(str(va_arg(print, char *)));
-						count = count + 1;
-					}
-					else
-					{
-						_putchar(format[i + 1]);
-						count = count + 1;
-					}
-					i++;
-				}
+				_putchar(format[i + 1]);
+				count = count + 1;
 			}
 			i++;
 		}
+		}
+		i++;
 	}
 	va_end(print);
 	return (count);
