8af6a5e241447aa315c6ba43041f39b3d7be14ed
samngoru <samngoru@gmail.com>
2023-04-24 10:53:35 -0700

    output according to format

--- printf.c
+++ printf.c
@@ -1,6 +1,7 @@
 #include <stdarg.h>
 #include "main.h"
 #include <stddef.h>
+#include <stdlib.h>
 /**
  * _printf - printing function
  * @format: variable
@@ -14,35 +15,37 @@ int _printf(const char *format, ...)
 	va_start(print, format);
 	if (format == NULL)
 		return (-1);
-	while (format[i] != '\0' && format != NULL)
-	{
-		if (format[i] == '%')
+	if (format != NULL)
+		while (format[i] != '\0')
 		{
-			format++;
-			switch (format[i])
+			if (format[i] == '%')
 			{
-				case 'c':
-					count += print_char(print);
-					break;
-				case 's':
-					count += print_string(print);
-					break;
-				case '%':
-					count += _putchar(format[i]);
-					break;
-				default:
-					_putchar('%');
-					_putchar(format[i]);
-					count += 2;
-					break;
+				format++;
+				switch (format[i])
+				{
+					case 'c':
+						count += print_char(print);
+						break;
+					case 's':
+						count += print_string(print);
+						break;
+					case '%':
+						count += _putchar(format[i]);
+						break;
+					default:
+						_putchar('%');
+						_putchar(format[i]);
+						count += 2;
+						break;
+				}
 			}
+			else
+			{
+				count += _putchar(format[i]);
+			}
+			i++;
 		}
-		else
-		{
-			count += _putchar(format[i]);
-		}
-		i++;
-	}
-	va_end(print);
-	return (count);
+		va_end(print);
+		return (count);
+	exit(1);
 }
