3f4ba557da937f5d5454fbd9d96d7d0df3baac76
samngoru <samngoru@gmail.com>
2023-04-24 10:18:12 -0700

    output according to format

--- printf.c
+++ printf.c
@@ -9,44 +9,41 @@
  */
 int _printf(const char *format, ...)
 {
-	if (format != NULL)
+	int i = 0, count = 0;
+	va_list print;
+
+	va_start(print, format);
+	if (format == NULL)
+		return (-1);
+	while (format[i] != '\0')
 	{
-		int i = 0, count = 0;
-		va_list print;
-
-		va_start(print, format);
-
-		while (format[i] != '\0')
+		if (format[i] == '%')
 		{
-			if (format[i] == '%')
+			format++;
+			switch (format[i])
 			{
-				format++;
-				switch (format[i])
-				{
-					case 'c':
-						count += print_char(print);
-						break;
-					case 's':
-						count += print_string(print);
-						break;
-					case '%':
-						count += _putchar(format[i]);
-						break;
-					default:
-						_putchar('%');
-						_putchar(format[i]);
-						count += 2;
-						break;
-				}
+				case 'c':
+					count += print_char(print);
+					break;
+				case 's':
+					count += print_string(print);
+					break;
+				case '%':
+					count += _putchar(format[i]);
+					break;
+				default:
+					_putchar('%');
+					_putchar(format[i]);
+					count += 2;
+					break;
 			}
-			else
-			{
-				count += _putchar(format[i]);
-			}
-			i++;
 		}
-		va_end(print);
-		return (count);
+		else
+		{
+			count += _putchar(format[i]);
+		}
+		i++;
 	}
-	exit(1);
+	va_end(print);
+	return (count);
 }
